f780ca3c45ec4bac2fcdf8d9d016e6febc600753
adamaconguli <waltson2003@gmail.com>
2023-09-30 22:34:59 +0800

    lesson 7: styles & style sheet

--- App.js
+++ App.js
@@ -2,19 +2,18 @@ import { useState } from 'react'
 import { StyleSheet, Text, View, Button, Linking } from 'react-native'
 
 export default function App() {
-  const [count, setCount] = useState(0)
-  const [clicks, setClicks] = useState(0)
+  const [name, setName] = useState('Style Text')
 
   const onClickhandler = () => {
-    setCount((prevCount) => prevCount + 5)
-    setClicks((prevClicks) => prevClicks + 1)
+    setName('Style Test is Done!')
   }
 
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>{count}</Text>
-      <Button title='add' onPress={onClickhandler}></Button>
-      <Text style={styles.text}>You clicked {clicks} times</Text>
+      <Text style={styles.text}>{name}</Text>
+      <View style={styles.button}>
+        <Button title='Add' onPress={onClickhandler}></Button>
+      </View>
     </View>
   )
 }
@@ -22,14 +21,22 @@ export default function App() {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: '#0000ff',
+    backgroundColor: '#ffff00',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 10,
+    borderColor: '#ff00ff',
+    borderRadius: 10,
   },
   text: {
-    color: '#ffffff',
+    color: '#000000',
     fontSize: 20,
     fontStyle: 'italic',
     margin: 10,
+    textTransform: 'uppercase',
+  },
+  button: {
+    width: 200,
+    height: 60,
   },
 })
