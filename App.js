d12dce9671d595ac020c69661eb512f5617facaa
adamaconguli <waltson2003@gmail.com>
2023-09-30 22:19:46 +0800

    lesson 6: using the state hook

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
+import { useState } from 'react'
 import { StyleSheet, Text, View, Button, Linking } from 'react-native'
 
 export default function App() {
+  const [count, setCount] = useState(0)
+  const [clicks, setClicks] = useState(0)
+
+  const onClickhandler = () => {
+    setCount((prevCount) => prevCount + 5)
+    setClicks((prevClicks) => prevClicks + 1)
+  }
+
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Just start learning react native</Text>
-      <Button title='github repo' onPress={() => Linking.openURL('https://github.com/waltsonzh/reactnativecourse')}></Button>
+      <Text style={styles.text}>{count}</Text>
+      <Button title='add' onPress={onClickhandler}></Button>
+      <Text style={styles.text}>You clicked {clicks} times</Text>
     </View>
   )
 }
@@ -21,5 +31,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontStyle: 'italic',
     margin: 10,
-  }
+  },
 })
